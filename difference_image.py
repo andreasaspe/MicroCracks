8646d1525159916f189c6f138840fa9b5d07a303
andreasaspe <awias@dtu.dk>
2024-08-22 15:28:16 +0200

    CRAZY! Har ændret på differencebilledet nu til at tage et gennemsnit og det fungerer sygt godt. Bare se bor fra block, bruger en block size på 1. 100 gange bedre at gennemsnitte over billeder end block size

--- difference_image.py
+++ difference_image.py
@@ -28,18 +28,22 @@ i = 0
 min_val = []
 max_val = []
 
-
 idx_list = [0,13700]   #idx_list = [0,13700] KANON! Sammenlign de to
 images = []
+avg_image = None
+start_idx = 0
+N_avg_images = 50
 
 # Åbn TIFF-filen
 with Image.open(tiff_file) as img:
-    for i in idx_list:
+    #Get average image for the first 20 images.
+
+    for i in range(start_idx,start_idx+N_avg_images):
         img.seek(i)  # Skift til den næste frame
         img_frame = img.copy()  # Kopi af den aktuelle frame
 
         # Konverter til NumPy array
-        img_array = np.array(img_frame)
+        img_array = np.array(img_frame,dtype=np.float64)
         # print(img_array.shape)
         # img_array[70,:] = 0
         # img_array[945+1,:] = 0
@@ -50,9 +54,10 @@ with Image.open(tiff_file) as img:
         # min_val.append(np.min(img_array_cropped))
         # max_val.append(np.max(img_array_cropped))
 
-        images.append(img_array_cropped)
-
-        # cmap='viridis'
+        if avg_image is None:
+            avg_image = img_array_cropped
+        else:
+            avg_image += img_array_cropped
 
         # fig = plt.figure()
         # im = plt.imshow(img_array_cropped,cmap=cmap, vmin = 4000, vmax = 10000)#vmin = 5513, vmax = 15000) #vmin=5570, vmax = 58631) #vmin er mindste af alle værdier of vmax er max af alle værdier
@@ -61,6 +66,70 @@ with Image.open(tiff_file) as img:
         # plt.show()
         # plt.close()
 
+#Calculate average image
+avg_image /= N_avg_images
+avg_image1 = avg_image
+
+#Plot average image
+cmap='viridis'
+fig = plt.figure()
+im = plt.imshow(avg_image1,cmap=cmap)#vmin = 5513, vmax = 15000) #vmin=5570, vmax = 58631) #vmin er mindste af alle værdier of vmax er max af alle værdier
+fig.colorbar(im, orientation='vertical')
+plt.title(i)
+plt.show()
+plt.close()
+
+
+
+#Another avg_image
+avg_image = None
+start_idx = 13500
+N_avg_images = 50
+
+# Åbn TIFF-filen
+with Image.open(tiff_file) as img:
+    #Get average image for the first 20 images.
+
+    for i in range(start_idx,start_idx+N_avg_images):
+        img.seek(i)  # Skift til den næste frame
+        img_frame = img.copy()  # Kopi af den aktuelle frame
+
+        # Konverter til NumPy array
+        img_array = np.array(img_frame,dtype=np.float64)
+        # print(img_array.shape)
+        # img_array[70,:] = 0
+        # img_array[945+1,:] = 0
+        # img_array[:,770] = 0
+        # img_array[:,115-1+6] = 0
+
+        img_array_cropped = img_array[70:945+1,220-1:685+2]
+        # min_val.append(np.min(img_array_cropped))
+        # max_val.append(np.max(img_array_cropped))
+
+        if avg_image is None:
+            avg_image = img_array_cropped
+        else:
+            avg_image += img_array_cropped
+
+        # fig = plt.figure()
+        # im = plt.imshow(img_array_cropped,cmap=cmap, vmin = 4000, vmax = 10000)#vmin = 5513, vmax = 15000) #vmin=5570, vmax = 58631) #vmin er mindste af alle værdier of vmax er max af alle værdier
+        # fig.colorbar(im, orientation='vertical')
+        # plt.title(i)
+        # plt.show()
+        # plt.close()
+
+#Calculate average image
+avg_image /= N_avg_images
+avg_image2 = avg_image
+
+#Plot average image
+cmap='viridis'
+fig = plt.figure()
+im = plt.imshow(avg_image2,cmap=cmap)#vmin = 5513, vmax = 15000) #vmin=5570, vmax = 58631) #vmin er mindste af alle værdier of vmax er max af alle værdier
+fig.colorbar(im, orientation='vertical')
+plt.title(i)
+plt.show()
+plt.close()
 
 
 
@@ -86,8 +155,8 @@ def block_mean_diff(array1, array2, block_size):
 
 
 
-block_size = 12
-img_diff = block_mean_diff(images[0], images[1], block_size)
+block_size = 1
+img_diff = block_mean_diff(avg_image1, avg_image2, block_size)
 
 # print(img_diff)
 
@@ -98,11 +167,13 @@ cbar = fig.colorbar(cax, ax=ax, orientation='vertical')
 cbar.set_label('Colorbar Label')  # Optional: Label for the colorbar
 plt.show()
 
-img_diff = images[0] - images[1]
 
-fig, ax = plt.subplots()
-cmap = 'viridis'
-cax = ax.imshow(img_diff, cmap=cmap)
-cbar = fig.colorbar(cax, ax=ax, orientation='vertical')
-cbar.set_label('Colorbar Label')  # Optional: Label for the colorbar
-plt.show()
+
+# # img_diff = images[0] - images[1]
+
+# # fig, ax = plt.subplots()
+# # cmap = 'viridis'
+# # cax = ax.imshow(img_diff, cmap=cmap)
+# # cbar = fig.colorbar(cax, ax=ax, orientation='vertical')
+# # cbar.set_label('Colorbar Label')  # Optional: Label for the colorbar
+# # plt.show()

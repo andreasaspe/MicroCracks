a13eed35ea415098112442ea37964cc1d91a899a
Andreas With Aspe <awias@dtu.dk>
2024-09-05 10:13:10 +0200

    elastix script updated

--- itk_elastix_test.py
+++ itk_elastix_test.py
@@ -1,3 +1,9 @@
 import itk
 
 print("Hello")
+
+
+fixed_image = itk.imread('/Volumes/T9/MicroCracks/pRESSURE/NIFTI/Pressure_tests_Scan_2_5_recon.nii')
+moving_image = itk.imread('/Volumes/T9/MicroCracks/pRESSURE/NIFTI/Pressure_tests_Scan_2_65_recon.nii')
+
+registered_image, params = itk.elastix_registration_method(fixed_image, moving_image)

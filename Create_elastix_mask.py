b4a88b7fb91acebec4e43b01c102f1908be6089a
Andreas With Aspe <awias@dtu.dk>
2024-09-09 23:36:02 +0200

    Dokumenter til nu computer, har intet med MicroCracks at gøre. Sletter dem igen i morgen, blot en overførselsmetode

--- Create_elastix_mask.py
+++ Create_elastix_mask.py
@@ -18,13 +18,12 @@ file2 = 'Pressure_tests_Scan_2_65_recon'
 
 #First file
 img_nib1 = nib.load(os.path.join(data_path,file1+'.nii'))
-# img_nib2 = nib.load(os.path.join(data_path,file1))
 
 img_data1 = img_nib1.get_fdata()
 
 mask1 = np.ones(img_data1.shape)
 
-mask1[img_data1 < 25000] = 0
+mask1[img_data1 < 30000] = 0
 
 nifti_image = nib.Nifti1Image(mask1, affine=np.eye(4))
 
@@ -35,11 +34,13 @@ print("NIFTI saved")
 
 
 #Second file
+img_nib2 = nib.load(os.path.join(data_path,file2+'.nii'))
+
 img_data2 = img_nib2.get_fdata()
 
 mask2 = np.ones(img_data2.shape)
 
-mask2[img_data2 < 25000] = 0
+mask2[img_data2 < 30000] = 0
 
 nifti_image = nib.Nifti1Image(mask2, affine=np.eye(4))
 

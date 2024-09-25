20d5464514d834a857de530b3e5026e3a0a2a105
Andreas With Aspe <awias@dtu.dk>
2024-09-25 16:46:53 +0200

    Ændrede check source scriptet

--- Check_source_with_square.py
+++ Check_source_with_square.py
@@ -12,9 +12,10 @@ import matplotlib.pyplot as plt
 import os
 import shutil
 from tqdm import tqdm
+import matplotlib.patches as patches
 import subprocess
 
-tiff_file = 'g:\MicroCracks\Injection_tracer_test_2D_overnight.ome.tiff'
+tiff_file = r'C:\Users\awias\Documents\Research_Assistant\MicroCracks\Data\Injection_tracer_test_2D_overnight.ome.tiff'
 # tiff_file = 'g:\MicroCracks\Injection_tracer_test_2D_overnight_high_pressure.ome.tiff'
 
 i = 0
@@ -29,6 +30,7 @@ with Image.open(tiff_file) as img:
     # Læs hver side/billede i TIFF-filen
     # for i in range(img.n_frames):
     while i < img.n_frames:
+        print(i)
         
         img.seek(i)  # Skift til den næste frame
         img_frame = img.copy()  # Kopi af den aktuelle frame
@@ -45,17 +47,22 @@ with Image.open(tiff_file) as img:
 
         cmap='viridis'
 
-
-        # fig = plt.figure()
-        # im = plt.imshow(img_array_cropped,cmap=cmap) # vmin = 4000, vmax = 10000 #vmin = 5513, vmax = 15000) #vmin=5570, vmax = 58631) #vmin er mindste af alle værdier of vmax er max af alle værdier
-        # fig.colorbar(im, orientation='vertical')
-        # plt.title(i)
-        # plt.show()
-        # plt.close()
+        
+        fig, ax = plt.subplots()
+        im = ax.imshow(img_array,cmap=cmap) # vmin = 4000, vmax = 10000 #vmin = 5513, vmax = 15000) #vmin=5570, vmax = 58631) #vmin er mindste af alle værdier of vmax er max af alle værdier
+        # Opret en rød rektangel (xy-koordinat er toppen venstre hjørne, bredde og højde følger efter)
+        rect = patches.Rectangle((880, 70), 20, 20, linewidth=2, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        fig.colorbar(im, orientation='vertical')
+        plt.title(i)
+        plt.show()
+        plt.close()
 
         mean = np.mean(img_array_cropped)
         mean_list.append(mean)
 
         i+= 1
 
-print()
+plt.figure()
+plt.hist(mean_list,bins=100)
+plt.show()

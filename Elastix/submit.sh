21ec058323b028eec3218987ec91730bcecf30a4
andreasaspe <awias@dtu.dk>
2024-10-01 11:37:08 +0200

    Måske virker denne merge? Håber

--- Elastix/submit.sh
+++ Elastix/submit.sh
@@ -3,13 +3,13 @@
 ### -- specify queue -- 
 #BSUB -q hpc
 ### -- set the job Name -- 
-#BSUB -J My_Application
+#BSUB -J Elastix_job
 ### -- ask for number of cores (default: 1) -- 
 #BSUB -n 4 
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=4GB]"
+#BSUB -R "rusage[mem=16GB]"
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
 #BSUB -M 5GB
 ### -- set walltime limit: hh:mm -- 
@@ -17,7 +17,7 @@
 ### -- set the email address -- 
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
-##BSUB -u your_email_address
+##BSUB -u awias@dtu.dk
 ### -- send notification at start -- 
 #BSUB -B 
 ### -- send notification at completion -- 
@@ -28,4 +28,6 @@
 #BSUB -e Output_%J.err 
 
 # here follow the commands you want to execute with input.in as the input file
-myapplication.x input.in > output.out
+export LD_LIBRARY_PATH=/dtu/3d-imaging-center/QIM/elastix/elastix-4.9.0/lib/:$LD_LIBRARY_PATH
+
+/dtu/3d-imaging-center/QIM/elastix/elastix-4.9.0/bin/elastix -f /work3/awias/MicroCracks/fixed_cropped.nii -m /work3/awias/MicroCracks/moved_cropped.nii -fMask /work3/awias/MicroCracks/fixed_cropped_mask.nii -mMask /work3/awias/MicroCracks/moved_cropped_mask.nii -out /work3/awias/MicroCracks/Outputs -p /work3/awias/MicroCracks/parameterFile.txt
